9c11cc5d771d9bdeb3268570543852846fa1ce23
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2022-05-10 12:59:39 -0300

    Iniciando a personalizacao dos itens no player ativo

--- vuejs/src/components/players/ActivePlayer.vue
+++ vuejs/src/components/players/ActivePlayer.vue
@@ -1,24 +1,68 @@
 <template>
   <div>
-    <v-row class="mb-2" style="background-color: green; height: 100px">
+    <div style="background-color: green; border-radius: 5px">
       <div class="d-flex justify-space-around align-center">
-        <div class="mr-3">
+        <div :style="{ 'margin-left': margin_between_stats + 'px ' }">
           <div class="d-flex justify-content-between align-center">
-            <img class="mr-3" src="../../assets/health.png" alt="" width="20" />
-            <div>100</div>
+            <img src="../../assets/health.png" alt="" width="20" />
+            <div
+              :style="{
+                'margin-left': margin_between_icon_and_stats + 'px !important',
+              }"
+            >
+              100
+            </div>
           </div>
         </div>
-        <div class="mr-3">colete : true</div>
-        <div class="mr-3">kills : 10</div>
+        <div :style="{ 'margin-left': margin_between_stats + 'px ' }">
+          <img
+            class="mr-3 mt-1"
+            src="../../assets/active_player_icons/Armor_hud_css.webp"
+            alt=""
+            width="20"
+          />
+        </div>
+        <div
+          :style="{
+            'margin-left': margin_between_stats + 'px ',
+          }"
+        >
+          K - 10
+        </div>
 
-        <div class="mr-3">mortes : 10</div>
-        <div class="mr-3">Munição: 30</div>
-        <div class="mr-3">Total: 90</div>
+        <div :style="{ 'margin-left': margin_between_stats + 'px ' }">
+          D - 10
+        </div>
+        <div
+          class="mr-3 d-flex jusity-space-between"
+          :style="{ 'margin-left': margin_between_stats + 'px ' }"
+        >
+          <img
+            class="mr-3"
+            src="../../assets/active_player_icons/ammo.png"
+            alt=""
+            width="20"
+          />
+          <div
+            :style="{
+              'margin-left': margin_between_icon_and_stats + 'px !important',
+            }"
+          >
+            30 / 90
+          </div>
+        </div>
       </div>
-    </v-row>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      margin_between_stats: 40,
+      margin_between_icon_and_stats: 10,
+    };
+  },
+};
 </script>
 <style></style>
