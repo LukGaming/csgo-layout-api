c9ab384930050228b2743f99427104984fc75ec6
Paulo Antonio Mendes <thelukgaming333@gmail.com>
2022-05-12 21:11:10 -0300

    Adicionando Konva Js e testando dois triangulos

--- vuejs/src/components/Map.vue
+++ vuejs/src/components/Map.vue
@@ -3,8 +3,9 @@
     <template>
       <v-stage :config="configKonva">
         <v-layer>
+          <v-regular-polygon :config="regularPolygon" ref="triangle"></v-regular-polygon>
+          <!-- <v-shape :config="test" ref="triangle"></v-shape> -->
           <v-circle :config="configCircle"></v-circle>
-          <v-shape :config="test"></v-shape>
         </v-layer>
       </v-stage>
     </template>
@@ -14,6 +15,7 @@
 export default {
   data() {
     return {
+      nodes: null,
       configKonva: {
         x: 100,
         y: 100,
@@ -25,26 +27,45 @@ export default {
         y: 100,
         radius: 70,
         fill: "red",
-        stroke: "black",
-        strokeWidth: 4,
+        width: 15,
+        height: 15,
       },
       test: {
-        sceneFunc: function(context, shape) {
+        sceneFunc: function (context, shape) {
           context.beginPath();
-          context.moveTo(0, 150);
-          context.lineTo(150, 50);
-          context.quadraticCurveTo(150,150,150,150);
+          context.moveTo(0, 8);
+          context.lineTo(8, 0);
+          context.quadraticCurveTo(10, 10, 10, 10);
           context.closePath();
-
-          // special Konva.js method
           context.fillStrokeShape(shape);
         },
-        fill: '#00D2FF',
-        stroke: 'black',
-        strokeWidth: 0
-      }
-      
+        fill: "#00D2FF",
+        x: 100,
+        y: 100,
+        points: 0,
+      },
+      regularPolygon: {
+        x: 100,
+        y: 95,
+        sides: 3,
+        radius: 10,
+        fill: "yellow",
+        stroke: "black",
+        strokeWidth: 4,
+        offsetX: 10,
+        offsetY: 10
+        
+      },
     };
+  },
+  created() {
+    setTimeout(() => {
+      this.nodes = this.$refs.triangle.getNode();
+      console.log(this.nodes);
+      setInterval(() => {
+        this.nodes.rotation(-132);
+      }, 100);
+    }, 1000);
   },
 };
 </script>
